e1749308de48e7d9a145cb8e38f7748f17764010
Edgar Michael Pérez Valdez <michael.valdezz@yahoo.com>
2025-02-05 22:18:09 -0600

    Cambios de rutas en los paquetes e imports

--- app/src/main/java/com/desswapp/module4eqp2/excercise/home/allison_olvera/InitialActivityAMOM.kt
+++ app/src/main/java/com/desswapp/module4eqp2/excercise/home/allison_olvera/InitialActivityAMOM.kt
@@ -1,4 +1,4 @@
-package com.example.androidmodule1.exercise.home.AllisonOlvera
+package com.desswapp.module4eqp2.excercise.home.allison_olvera
 
 import android.content.Intent
 import android.net.Uri
@@ -11,7 +11,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.androidmodule1.R
+import com.desswapp.module4eqp2.R
 
 class InitialActivityAMOM : AppCompatActivity() {
 
--- app/src/main/java/com/desswapp/module4eqp2/excercise/home/allison_olvera/SecondActivityAMOM.kt
+++ app/src/main/java/com/desswapp/module4eqp2/excercise/home/allison_olvera/SecondActivityAMOM.kt
@@ -1,4 +1,4 @@
-package com.example.androidmodule1.exercise.home.AllisonOlvera
+package com.desswapp.module4eqp2.excercise.home.allison_olvera
 
 import android.content.Intent
 import android.os.Bundle
@@ -8,7 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.androidmodule1.R
+import com.desswapp.module4eqp2.R
 
 class SecondActivityAMOM : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
