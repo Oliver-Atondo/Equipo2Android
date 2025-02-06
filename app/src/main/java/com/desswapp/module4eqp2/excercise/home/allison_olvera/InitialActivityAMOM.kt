ad9a83a566c530f6253b24fec6964b73b1a53796
AllisonOM <allisonm1236@gmail.com>
2025-02-05 20:52:52 -0600

    Ejercicios 1 Allison Olvera

---
+++ app/src/main/java/com/desswapp/module4eqp2/excercise/home/allison_olvera/InitialActivityAMOM.kt
@@ -0,0 +1,90 @@
+package com.example.androidmodule1.exercise.home.AllisonOlvera
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.androidmodule1.R
+
+class InitialActivityAMOM : AppCompatActivity() {
+
+    private val register = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+        if (result.resultCode == RESULT_OK){
+            val isCorrect = result.data?.getBooleanExtra("EXTRA_IS_CORRECT", false)
+            if (isCorrect == true)
+                Toast.makeText(this, "Información regresada", Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(this, "Error de información regresada", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "CANCELED", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_initial_amom)
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnSpotify = findViewById<Button>(R.id.btnSpotify)
+        val btnOpenSecondScreen = findViewById<Button>(R.id.btnOpenSecondScreen)
+
+        btnSpotify.setOnClickListener {
+            val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://open.spotify.com/intl-es"))
+            startActivity(Intent.createChooser(urlIntent, "Abrir usando:"))
+        }
+
+        btnOpenSecondScreen.setOnClickListener {
+            val secondIntent = Intent(this, SecondActivityAMOM::class.java).apply {
+                putExtra("EXTRA_NAME", "Allison Olvera")
+                putExtra("EXTRA_AGE", 23)
+                putExtra("EXTRA_GENDER", 'F')
+                putExtra("EXTRA_DECEASED", true)
+            }
+            register.launch(secondIntent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show()
+    }
+
+}
---
+++ app/src/main/java/com/desswapp/module4eqp2/excercise/home/allison_olvera/SecondActivityAMOM.kt
@@ -0,0 +1,58 @@
+package com.example.androidmodule1.exercise.home.AllisonOlvera
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.androidmodule1.R
+
+class SecondActivityAMOM : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second_amom)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val tvName = findViewById<TextView>(R.id.tvName)
+        val tvAge = findViewById<TextView>(R.id.tvAge)
+        val tvGender = findViewById<TextView>(R.id.tvGender)
+        val tvDeceased = findViewById<TextView>(R.id.tvDeceased)
+        val btnReturnInfo = findViewById<Button>(R.id.btnReturnInfo)
+
+        intent.extras?.let {
+            if (it.containsKey("EXTRA_NAME")){
+                val name = it.getString("EXTRA_NAME")
+                tvName.text = "Nombre: $name"
+            }
+            if (it.containsKey("EXTRA_AGE")){
+                val age = it.getInt("EXTRA_AGE")
+                tvAge.text = "Edad: " + age.toString()
+            }
+            if (it.containsKey("EXTRA_GENDER")) {
+                val gender = it.getChar("EXTRA_GENDER")
+                tvGender.text = "Género: " + gender.toString()
+            }
+            if (it.containsKey("EXTRA_DECEASED")) {
+                val deceased = it.getBoolean("EXTRA_DECEASED")
+                val deceasedStr = if (deceased) "Vivo" else "Finado"
+                tvDeceased.text = "Estado actual de la persona: " + deceasedStr
+            }
+        }
+
+        btnReturnInfo.setOnClickListener {
+            val resultIntent = Intent().apply {
+                putExtra("EXTRA_IS_CORRECT", true)
+            }
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
+    }
+}
