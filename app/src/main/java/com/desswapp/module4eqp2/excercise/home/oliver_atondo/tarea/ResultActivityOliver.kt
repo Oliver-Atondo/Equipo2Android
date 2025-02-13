7f9a40e404d2284f5681f8a1e2a5f19fe8c2bd63
Oliver-Atondo <oa0323@dexcom.com>
2025-02-13 03:40:34 -0800

    Tarea
    Un avance del 70%

---
+++ app/src/main/java/com/desswapp/module4eqp2/excercise/home/oliver_atondo/tarea/ResultActivityOliver.kt
@@ -0,0 +1,28 @@
+package com.desswapp.module4eqp2.excercise.home.oliver_atondo.tarea
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.desswapp.module4eqp2.R
+
+class ResultActivityOliver : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_oliver)
+
+        val nombreTextView = findViewById<TextView>(R.id.textNombre)
+        val apellidoTextView = findViewById<TextView>(R.id.textApellido)
+        val correoTextView = findViewById<TextView>(R.id.textCorreo)
+        val sexoTextView = findViewById<TextView>(R.id.textSexo)
+
+        val nombre = intent.getStringExtra("nombre")
+        val apellido = intent.getStringExtra("apellido")
+        val correo = intent.getStringExtra("correo")
+        val sexo = intent.getStringExtra("sexo")
+
+        nombreTextView.text = "Nombre: $nombre"
+        apellidoTextView.text = "Apellido: $apellido"
+        correoTextView.text = "Correo: $correo"
+        sexoTextView.text = "Sexo: $sexo"
+    }
+}
